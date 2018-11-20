295bbcaeb8b741c381590ac7dfc07fd48d81d5a7
Benson Wu <bensontwu@benson-mbp.local>
2018-11-19 16:11:48 -0800

    Initial Commit

---
+++ strengthfit/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  strengthfit
+//
+//  Created by Benson Wu on 11/19/18.
+//  Copyright © 2018 Benson Wu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
